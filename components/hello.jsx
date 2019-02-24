2f6408887370c57d1ff50cfaedf99041a781fc58
caicai <clq1@qq.com>
2019-02-25 00:06:28 +0800

    mod

--- components/hello.jsx
+++ components/hello.jsx
@@ -1,62 +1,80 @@
 import React from 'react';
 import axios from 'axios';
-import {Form, Icon, Input, Button, Checkbox,} from "antd";
+import {Row, Col, Table, Form, Icon, Input, Button, Checkbox, message} from "antd";
 
 class HelloComponent extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            listData: []
+            userInfo: []
         };
     }
 
     async componentDidMount() {
-/*        await axios.get('/list')
-            .then(res => {
-                const listData = res.data.data.list;
-                this.setState({
-                    listData
-                });
-            });*/
+
     }
 
-    submitForm() {
-        axios.get('/user', {
-            params: {
-                id: document.getElementById('id').value
-                // name: document.getElementById('name').value,
-            }
-        }).then(res => {
-                // const userInfo = res.data.data.user
-                // this.setState({
-                //     userInfo
-                // })
-            }
-        ).catch(err => console.log(err));
+    //这里  e=>{} 解决this  undefind的问题
+    submitForm = (e) => {
+        let _id = document.getElementById('id').value
+        if (_id) {
+            axios.get('/user', {
+                params: {
+                    id: _id
+                }
+            }).then(res => {
+                    const userInfo = res.data.data.user
+                    this.setState({
+                        userInfo
+                    })
+                }
+            ).catch(err => console.log(err));
+        } else {
+            message.warning('请输入id');
+        }
+
     }
 
     render() {
-        const listData = this.state.listData;
-        let ele = listData.map(item => {
-            return <p key={item.id}>{item.id}:{item.title}</p>;
-        });
+        let userInfo = this.state.userInfo
 
-        // let userInfo = this.state.userInfo
-        // let user = userInfo.map(item => {
-        //     return <p key={item.id}>{item.uid}:{item.mobile}</p>
-        // })
+        const columns = [{
+            title: 'id',
+            dataIndex: 'id',
+            key: 'id',
+        }, {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+        }, {
+            title: '年龄',
+            dataIndex: 'age',
+            key: 'age',
+        }];
+
+        console.log(userInfo)
+        let user = userInfo.map(item => {
+            return <li key={item.id}>{item.id}:{item.name}-{item.age}</li>
+        })
         return (
             <div>
-                <div>{ele}</div>
-                {/*<div>{user}</div>*/}
-                <Form layout="inline">
-                    <Form.Item>
-                        <Input type="text" name="id" id="id"/>
-                    </Form.Item>
-                    <Button icon="search" type="primary" onClick={this.submitForm}>search</Button>
-                </Form>
-
+                <Row>
+                    <Col span={5}>
+                        <Form layout="inline" style={{margin: '20px'}}>
+                            <Form.Item>
+                                <Input type="text" name="id" id="id"/>
+                            </Form.Item>
+                            <Button icon="search" type="primary" onClick={this.submitForm}>search</Button>
+                        </Form>
+                        <ul>{user}</ul>
+                    </Col>
+                    <Col span={19}>
+                        <Table rowKey={(record, index) => `complete${record.id}${index}`}
+                               dataSource={userInfo}
+                               columns={columns}/>
+                    </Col>
+                </Row>
 
             </div>
 
